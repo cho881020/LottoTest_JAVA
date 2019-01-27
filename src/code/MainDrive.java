c13b950bdf0d4817818a1211a4254e85689c7bc0
hurd0001 <47047131+hurd0001@users.noreply.github.com>
2019-01-27 15:24:45 +0900

    배열에 6 입력 숫자 입력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,8 +1,18 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 		
+		int[] num6 = new int[6];
+		Scanner scan = new Scanner(System.in);
+		
+		for (int i=0 ; i < num6.length ; i++) {
+			System.out.println("숫자 입력 =");
+			int inputnum = scan.nextInt();
+			num6[i] = inputnum;
+		}
 	}
 }
