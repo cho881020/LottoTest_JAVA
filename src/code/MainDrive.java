3554d4c78f0b661a3a47a72b87377d08b610d1cb
sunjeong55 <sunjeong55@naver.com>
2019-01-27 15:29:28 +0900

    숫자입력받아 저장

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,8 +1,30 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-//		배선정입니다
+		
+//		기능1. 사용자에게 6개 숫자를 입력받아서 배열에 저장.
+		Scanner scan = new Scanner(System.in);
+		
+		int[] userInputNumArr = new int[6];
+		System.out.print("숫자 6개를 입력하세요: ");
+	
+			
+			
+			for(int i=0; i<userInputNumArr.length;i++) {
+				int userInput = scan.nextInt();
+				userInputNumArr[i]=userInput;
+			}
+		
+			
+			for(int i=0; i<userInputNumArr.length;i++)
+				System.out.println(userInputNumArr[i]);
+				
+	
+
+	
 	}
 }
