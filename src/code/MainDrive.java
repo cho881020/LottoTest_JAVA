ca54a9d3683b180688245948b6a56c051ba08355
shairyu93 <shanav@naver.com>
2019-01-27 15:20:50 +0900

    6개의 번호 배열에 담기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,9 +1,26 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
 
-//		주석확인.		
+//		기능1. 사용자에게 6개의 숫자를 입력받아서 배열에 저장.
+//		작성 완료하면 커밋후 푸시.
+		
+		int[] userInput = new int[6];
+		
+		for(int i=0; i<userInput.length; i++) {
+			
+
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print(String.format("%d번째 번호를 입력하세요 : ", i+1));
+			
+			userInput[i] = sc.nextInt();
+			System.out.println(userInput[i]);
+		}
+
 	}
 }
