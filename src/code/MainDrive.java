43ae059ebecfa53c86ef625fd83941314a9ab4fc
monaru1124 <monaru1124@naver.com>
2019-01-27 16:00:07 +0900

    컴퓨터 당첨번호 생성, 중복검사 실행
    - 당첨번호가 저장될 배열 생성
    - 랜덤 번호를 1~45로 생성
    - 기존의 당첨번호와 중복되는지 검사
    - 검사를 통과하면 배열에 등록

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -42,8 +42,15 @@ public class MainDrive {
 					}
 			
 				}
+				
+//				범위와 중복 검사를 모두 (and) 통과한다면
 				if(numRange && isnotDuplNum) {
+					
+//					배열에 입력값을 저장.
 					userInputArr[i] = userInput;
+//					안내매세지 출력
+					System.out.println("다음 숫자를 입력하세요.");
+//					다음 숫자를 입력받기 위해 while문을 탈출
 					break;
 				}
 				else {
@@ -57,17 +64,61 @@ public class MainDrive {
 		} // for 끝 ==> 사용자 숫자 저장.
 		
 		
-		
-		
-		
-		
-		
-		
-
 //	 2) 컴퓨터가 이번주의 당첨번호 6개를 생성.
 //	 - 1~45의 숫자만 입력가능, 중복된 숫자는 허용 X
 //	 ※ 보너스번호는 이번 문제에서는 반영하지 않을 생각.
-//
+
+//		당첨번호 6개를 저장할 배열을 생성.
+		int[] lottoAnswerArr = new int[6];
+		
+		
+//		6개의 당첨번호를 만들기 위한 반복문.
+		for (int i = 0 ; i < lottoAnswerArr.length ; i++ ) {
+			
+			
+//			조건을 만족시킬때까지 다시 만들도록 무한반복.
+			while(true) {
+				double randomVal = Math.random();
+				
+//				0 + 1 <= randomVal * 45 + 1 < 45 + 1     => 1 <= randomVal < 46 => 1~45
+				int randomNum = (int) ( randomVal * 45 + 1 );
+				
+//				랜덤으로 나온 값이 1~45인지? 검사를 안해도 되겠다!
+//				boolean rangeOk = 
+				
+//				이미 만들어져있는지? 검사해야함.
+				boolean duplOk = true ;
+				
+				for (j=0 ; j < lottoAnswerArr.length ; j++) {
+					
+					if (lottoAnswerArr[j] == randomNum) {
+						duplOk = false ;
+						break;
+					}
+				}
+				
+//				중복여부가 괜찮다면
+				if (duplOk) {
+//					랜덤으로 생성한 번호를 당첨번호로 등록.
+					lottoAnswerArr[i] = randomNum ;
+//					다음 숫자를 뽑으러 이동.
+					break;
+				}
+				
+			}  // while 종료 => 숫자 생성 반복
+			
+		} // for 끝 
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 //	 3) 두 개의 배열을 모두 작은 숫자부터 큰 숫자 순으로 정렬.
 //
 //	4) 정렬된 두개의 배열을 가지고 비교해서 등수를 출력.
@@ -76,5 +127,5 @@ public class MainDrive {
 //	  - 4개 : 4등
 //	  - 3개 : 5등
 		
-	}
+	} // main 종료
 }
