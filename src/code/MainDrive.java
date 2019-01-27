19b2a3179d0d86d424392a3fe6b2444a25b8817b
sunjeong55 <sunjeong55@naver.com>
2019-01-27 15:35:03 +0900

    사용자에게 숫자 6개 입력받기

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -7,24 +7,16 @@ public class MainDrive {
 	public static void main(String[] args) {
 		
 //		기능1. 사용자에게 6개 숫자를 입력받아서 배열에 저장.
-		Scanner scan = new Scanner(System.in);
 		
+		Scanner scan = new Scanner(System.in);
 		int[] userInputNumArr = new int[6];
-		System.out.print("숫자 6개를 입력하세요: ");
-	
-			
-			
+
 			for(int i=0; i<userInputNumArr.length;i++) {
+				System.out.print(String.format("%d번째 숫자 입력: ", i+1));
 				int userInput = scan.nextInt();
-				userInputNumArr[i]=userInput;
+
 			}
 		
-			
-			for(int i=0; i<userInputNumArr.length;i++)
-				System.out.println(userInputNumArr[i]);
-				
-	
-
 	
 	}
 }
