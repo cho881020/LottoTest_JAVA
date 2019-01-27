a6bc027fde46ffc0a6d92670efcf1feb5b164e99
rosekathy <rosekathy@naver.com>
2019-01-27 15:30:34 +0900

    사용자에게 6개의 숫자를 입력받아서 배열에 저장

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,8 +1,22 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
+
+//		1) 사용자에게 6개의 숫자를 입력받아서 배열에 저장
+
+		int[] userInputNumbers = new int[6];
+
+		Scanner scan = new Scanner(System.in);
+
+		for (int i = 0; i < userInputNumbers.length; i++) {
+
+				System.out.print(String.format("%d번째 숫자입력 : ", i + 1));
+
+				userInputNumbers[i] = scan.nextInt();
+		}
 	}
 }
