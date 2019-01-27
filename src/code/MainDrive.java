74469f88d30d2cf9f4e45953ec3201481c09ad9d
sunjeong55 <sunjeong55@naver.com>
2019-01-27 16:00:07 +0900

    컴터 당첨번호 생성, 중복 검사 실행
    -당첨번호 저장될 배열 생성.
    -랜덤 번호 1~45로 생성
    -기존의 당첨 번호와 중복 되는가 검사후
    -검사 통과하면 배열에 등록

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -48,5 +48,40 @@ public class MainDrive {
 
 //		 기능 2. 6개의 숫자를 입력받을때 1~45 인지, 중복 검사해서 ok 일때만 배열에 저장.
 
+//		 기능 3. 컴퓨터가 6개의 숫자 저장. 1~45 인지, 중복 검사해서 ok 일때만 배열에 저장.
+		
+		
+		int[] lottoAnswerArr = new int[6];	//당첨번호 6개 저장할 배열
+		
+//		6개의 당첨번호를 만들기 위한 반복문
+		for(int i=0; i<lottoAnswerArr.length;i++) {
+			
+			while(true) { 	//어떤 조건을 만족시킬때까지 계속 다시 만들어야 하니까 while
+				double randomVal = Math.random();
+//				0<= randomVal <1
+//				randomVal 에 45을 곱한후 1 더하면 ? 0<= randomVal*45<45
+//				1<=randomVal*45+1<46 
+				
+				int randomNum = (int)(randomVal*45+1); 		//컴터가 1~45랜덤으로 만든다
+				
+//				랜덤으로 나온 값이 1~45인가? 위에서 범위를 이미 제한 했기 때문에 검사 안해도 될듯!
+//				boolean rangeOK - 검사 넘어감
+				
+//				중복은 ? 검사해야함.
+				boolean duplOk = true;
+				for(int j=0; j<lottoAnswerArr.length;j++) {
+					if(lottoAnswerArr[j]==randomVal) {
+						duplOk = false;
+						break;
+				}
+			}
+				if(duplOk) { //중복 여부 넘어가면
+					lottoAnswerArr[i] = randomNum; //랜덤 생성 번호를 당첨 번호로 등록
+					break;
+				}
+			
+			
+		}
+		
 	}
 }
