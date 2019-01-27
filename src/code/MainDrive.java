ebcac8d2db38a1f9574e6a2c9d21054d61b001bf
Psojeong <6muse.on@gmail.com>
2019-01-27 16:02:49 +0900

    당첨 번호 화면에 출력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -99,6 +99,11 @@ public class MainDrive {
 			}
 		}
 		
+//		당첨번호가 어떤 것들인지 화면에 출력
+		for (int i=0 ; i < lottoAnswerArr.length ; i++) {
+			System.out.println(lottoAnswerArr[i]);
+			
+		}
 		
 	}
 }
