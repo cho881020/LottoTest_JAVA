80b56908445ebfcc2d00d5cbdef5b538d80d5966
si15 <si09@naver.com>
2019-01-27 15:35:41 +0900

    6칸 숫자 입력

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -1,5 +1,7 @@
 package code;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
@@ -8,5 +10,24 @@ public class MainDrive {
 		
 //		기능1. 사용자에게 6개의 숫자를 입력받아서 배열에 저장
 //		작성 완료하면 커밋후 푸시
+		
+//		6칸 짜리 배열 생성.
+		
+			int[] userInputArr = new int[6];
+			
+			Scanner scan = new Scanner(System.in);
+			
+			for (int i = 0; i < userInputArr.length; i++) {
+//				숫자 입력 안내 메시지 띄우기
+				System.out.print(String.format("%d번째 숫자 입력 : ", i+1));
+				
+				userInputArr[i] = scan.nextInt();
+				
+			}
+			
+			
+
+
 	}
 }
+		
