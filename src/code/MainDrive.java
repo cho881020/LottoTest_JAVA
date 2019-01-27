df14f8486b822968c2bf9afd76c0326d4fc555b1
hurd0001 <47047131+hurd0001@users.noreply.github.com>
2019-01-27 16:18:58 +0900

    임시

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -6,13 +6,22 @@ public class MainDrive {
 
 	public static void main(String[] args) {
 		
-		int[] num6 = new int[6];
-		Scanner scan = new Scanner(System.in);
+//		int[] num6 = new int[6];
+//
+//		Scanner scan = new Scanner(System.in);
+//	 6개 숫자  입력 배열에 저장	
+//		for (int i=0 ; i < num6.length ; i++) {
+//			System.out.println(String.format("%d숫자 입력 =", i+1);
+//			int inputnum = scan.nextInt();
+//			num6[i] = inputnum;
+//		}
+////		출력
+//		for (int i=0 ; i < num6.length ; i++) {
+//			System.out.println(num6[i]);
+//		}
+
 		
-		for (int i=0 ; i < num6.length ; i++) {
-			System.out.println("숫자 입력 =");
-			int inputnum = scan.nextInt();
-			num6[i] = inputnum;
-		}
+		
+		
 	}
 }
