5ddc626a9c8ba7ff2920fd37e8af07d449fed1c7
si15 <si09@naver.com>
2019-01-27 15:46:00 +0900

    범위 검사 중복검사 통과해애 배열에 저장되도록 변경

--- src/code/MainDrive.java
+++ src/code/MainDrive.java
@@ -19,13 +19,40 @@ public class MainDrive {
 			
 			for (int i = 0; i < userInputArr.length; i++) {
 //				숫자 입력 안내 메시지 띄우기
-				System.out.print(String.format("%d번째 숫자 입력 : ", i+1));
+				while (true) {
+					System.out.print(String.format("%d번째 숫자 입력 : ", i+1));
+					
+//					입력값을 바로 배열에 넣지 않고, 별개 변수에 임시저장
+					int inputNum = scan.nextInt();
+					
+//					입력값이
+					boolean rangeOk = 1 <= inputNum && inputNum <= 45;
+//					중복검사 결과저장. 기본전제는 일단 true 설정
+					boolean duplOK = true;
+					
+					for (int j = 0; j < userInputArr.length; j++) {
+						if(userInputArr[j] == inputNum) {
+//							중복검사를 통과하지 못한다!
+							duplOK = false;
+//							하나라도 걸렸으면 더 볼필요도 없다
+							break;
+						}
+					}
+//				범위와, 중복검사를 모두 통과한다면
+					if(rangeOk && duplOK) {
+//						배열에 입력값읓ㄹ 저장
+						userInputArr[i] = inputNum;
+//						안내메시지 출력
+						System.out.println("다음 숫자를 입력하세요.");
+//						다음숫자를 입력하기 위해 while 문을 탈출
+						break;
+					}
+				}
 				
-				userInputArr[i] = scan.nextInt();
 				
 			}
 			
-			
+//			기능2. 6개의 숫자를 입력  받을때, 
 
 
 	}
